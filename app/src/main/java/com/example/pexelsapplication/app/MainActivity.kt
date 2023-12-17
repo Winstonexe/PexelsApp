6b1c601567ec4e3975989d107354c7de9a7bf7ba
Winston <128837084+Winstonexe@users.noreply.github.com>
2023-12-17 22:28:10 +0300

    Application finished! Was merged two branches and ended main activity.

--- app/src/main/java/com/example/pexelsapplication/app/MainActivity.kt
+++ app/src/main/java/com/example/pexelsapplication/app/MainActivity.kt
@@ -1,14 +1,48 @@
 package com.example.pexelsapplication.app
 
+import BottomBar
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.pexelsapplication.app.navigation.Screen
+import com.example.pexelsapplication.app.navigation.SetupNavGraph
+import com.example.pexelsapplication.ui.theme.PexelsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            PexelsAppTheme {
+                MyApp()
+            }
         }
+    }
+}
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    val items = listOf(Screen.Home, Screen.Bookmarks)
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    Scaffold(
+        bottomBar = {
+            if (currentRoute in items.map { it.route }) {
+                BottomBar(navController = navController)
+            }
+        }
+    ) {
+        SetupNavGraph(navController = navController, startDestination = Screen.Splash.route)
     }
 }
